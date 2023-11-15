a911afa5b4eb7a6ca3cc68a2ed6021da91fe9d8e
Anna Romanova <falsetheory@mail.ru>
2023-11-15 13:21:25 +0300

    bool replaced by enum, variables renamed, code style

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -4,76 +4,80 @@
 
 using namespace std;
 
-char field [3][3] = {{'.','.','.'},{'.','.','.'},{'.','.','.'}};
-// x
-// o
-bool userWin = false, compWin = false, noWin = false;
-int x, y;
-int h = 0;
-
-bool hasWon(char c){
+bool hasWon(char (&field)[3][3], char c) {
             // по строкам
     return  (field[0][0] == c && field[0][1] == c && field[0][2] == c) ||  
             (field[1][0] == c && field[1][1] == c && field[1][2] == c) ||
             (field[2][0] == c && field[2][1] == c && field[2][2] == c) ||
-             // по столбцам
+            // по столбцам
             (field[0][0] == c && field[1][0] == c && field[2][0] == c) ||  
             (field[0][1] == c && field[1][1] == c && field[2][1] == c) ||
-            (field[0][2] == c && field[2][1] == c && field[2][2] == c) ||
+            (field[0][2] == c && field[1][2] == c && field[2][2] == c) ||
             // по диагоналям
             (field[0][0] == c && field[1][1] == c && field[2][2] == c) ||
-            (field[0][2] == c && field[1][1] == c && field[2][0] == c) ;
-    
+            (field[0][2] == c && field[1][1] == c && field[2][0] == c) ; 
 }
 
-int main(){
+int main() {
+    char field [3][3] = {{'.','.','.'},{'.','.','.'},{'.','.','.'}};
+    const int MaxTurn = 9;
+
+    enum Result {undefined, userWin, compWin, noWin};
+
+    int x, y;
+    int turn = 0;
+    Result result = undefined;
 
     srand(time(NULL));
-    do{    
-        for(int i=0; i<3; i++)
-        {
-            for(int j=0; j<3; j++)
-            {
+    do {    
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<3; j++) {
                 cout << field[i][j];       
             }
             cout << endl;
         }
 
-        do
-        {
+        do {
             cout << "Enter position: ";
             cin >> x >> y;
         } while (field[x-1][y-1] != '.');       
         field[x-1][y-1]='x';
-        h++; 
+        turn++; 
         
-        if(h != 9){
-            do{
+        if (turn != MaxTurn) {
+            do {
                 x = rand()%3;   // 0-2
                 y = rand()%3;
             } while (field[x][y] != '.');
             field[x][y] = 'o';
-            h++;
+            turn++;
+        } 
+
+        if (hasWon(field, 'x')) {
+            result = userWin;
         }
-
-        userWin = hasWon('x');
-        compWin = hasWon('o');
-
-        if(!userWin && !compWin && h==9){
-            noWin = true;
+        if (hasWon(field, 'o')) {
+            result = compWin;
+        }    
+        if (result != userWin && result != compWin && turn == MaxTurn) {
+            result = noWin;
         }
 
         system("CLS");
-    } while(!userWin && !compWin && !noWin);
-
-    if(userWin){
-        cout << "userWin ";
-    }
-    if(compWin){
-        cout << "compWin ";
-    }
-    if(noWin){
-        cout << "noWin ";
+    } while (result == undefined);
+  
+    switch (result) {
+        case userWin:
+            cout << "userWin ";
+            break;
+        case compWin:
+            cout << "compWin ";
+            break;
+        case noWin:
+            cout << "noWin ";
+            break;    
+        default: cout << "UNDEFINED ";
+            break;
     }
 
     return 0;
